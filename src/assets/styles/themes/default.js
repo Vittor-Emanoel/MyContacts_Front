2608ca4a7fc1661813bc520a76489a5c449f3615
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-03-23 23:04:02 -0300

    :lipstick: FEAT: add style guide

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,24 @@
 export default {
-  backgroundColor: "#F6F5FC"
+  colors: {
+    background: "#F6F5FC",
+    primary: {
+      lighter: "#E0E3FF",
+      light: "#6674F4",
+      main: "#5061FC",
+      dark: "#3346F0"
+    },
+    success: {
+      main: "#51CA73", 
+    },
+    danger: {
+      light: "#F97171",
+      main: "#FC5050",
+      dark: "#F63131"
+    },
+    gray: {
+      gray_100: "#E6E6E6",
+      gray_200: "#BCBCBC",
+      gray_900:"#222222"
+    }
+  }
 };
