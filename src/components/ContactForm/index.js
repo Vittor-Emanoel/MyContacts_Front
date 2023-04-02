3ac49d056c42f0be3fd1bf9a70adb6454ab336f5
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-04-02 02:48:13 -0300

    FEAT: add validation in name input

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,28 +1,77 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import Button from '../Button';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 import { ButtonContainer, Form } from './styles';
 
+// Controlled Componentes = Responsabilidade do react, renderiza a cada letra
+// Uncontrolled Componentes = Javascript puro(useRef)
+
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  };
+
+  const handleNameChange = ({ target }) => {
+    setName(target.value);
+
+    if (!target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é Obrigatório' },
+      ]);
+    } else {
+      setErrors((prevState) => prevState.filter((error) => error.field !== 'name'));
+    }
+  };
+
+  console.log(errors);
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-
-      <FormGroup error="O formato do e-mail é inválido">
-        <Input placeholder="E-mail" error />
+        <Input placeholder="Nome" value={name} onChange={handleNameChange} />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={({ target }) => setPhone(target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={({ target }) => setCategory(target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">discord</option>
         </Select>
       </FormGroup>
 
