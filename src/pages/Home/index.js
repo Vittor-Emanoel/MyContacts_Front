6237383378802d17bf5bc4e41502619a1b083f5f
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-04-05 12:53:38 -0300

    FEAT: Rendering the data returned from the API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Card,
@@ -12,13 +13,29 @@ import edit from '../../assets/icons/Edit.svg';
 import trash from '../../assets/icons/Trash.svg';
 
 export default function Home() {
+  const [contacts, setContacts] = useState([])
+ 
+  useEffect(() => {
+    fetch("http://localhost:3001/contacts")
+        .then(async (response) => {
+        const json = await response.json()
+        setContacts(json)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  console.log(contacts);
   return (
     <Container>
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
       </InputSearchContainer>
       <Header>
-        <strong>3 contatos</strong>
+        <strong>{contacts.length} 
+        {contacts.length === 1 ? " contato" : " contatos"}
+        </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
@@ -30,39 +47,35 @@ export default function Home() {
           </button>
         </header>
 
-        <Card>
-          <div className="info">
-            <div className="contact-name">
-              <strong>Vittor Emanoel</strong>
-              <small>instagram</small>
-            </div>
-            <span>vittore.dev@gmail.com</span>
-            <span>(11) 99999-9999</span>
-          </div>
+       {contacts.map((contact) => (
+         <Card key={contact.id}>
+         <div className="info">
+           <div className="contact-name">
+             <strong>{contact.name}</strong>
+             {contact.category_name && (
+              <small>{contact.category_name}</small>
+             )}
+           </div>
+           <span>{contact.email}</span>
+           <span>{contact.phone}</span>
+         </div>
 
-          <div className="actions">
-            <Link to="/edit/1212">
-              <img src={edit} alt="edit" />
-            </Link>
-            <button type="button">
-              <img src={trash} alt="deleted" />
-            </button>
-          </div>
-        </Card>
+         <div className="actions">
+           <Link to={`/edit/${contact.id}`}>
+             <img src={edit} alt="edit" />
+           </Link>
+           <button type="button">
+             <img src={trash} alt="deleted" />
+           </button>
+         </div>
+       </Card>
+       ))}
       </ListContainer>
     </Container>
   );
 }
 
-fetch("http://localhost:3001/contacts")
-.then(async (response) => {
- const json = await response.json()
-  console.log(response)
-  console.log(json)
-})
-.catch((error) => {
-  console.log(error)
-})
+
 
 
 // SOP ==> Same Origin Policy -> Política de mesma origem
