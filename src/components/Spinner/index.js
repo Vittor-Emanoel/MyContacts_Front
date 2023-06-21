0f0b7c4f3d8d62e74b46df4b1aa465a65bbe6d9c
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-06-20 22:29:50 -0300

    REFACT: loader component

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,3 +1,14 @@
-export default function Spinner() {
-  return null
+import PropTypes from "prop-types"
+import { StyledSpinner } from "./styles"
+
+export default function Spinner({ size }) {
+  return <StyledSpinner size={size} />
+}
+
+Spinner.propTypes = {
+  size: PropTypes.number
+}
+
+Spinner.defaultProps = {
+  size: 32
 }
