d2806a08c688d563ed4dc605364718a732ede640
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-08-29 22:52:20 -0300

    feat: impletements toast with custom event

--- src/components/Toast/ToastMessage/index.js
+++ src/components/Toast/ToastMessage/index.js
@@ -4,21 +4,27 @@ import { Container } from "./styles"
 import xCircleIcon from "../../../assets/icons/x-circle.svg"
 import checkCircleIcon from "../../../assets/icons/check-circle.svg"
 
-export default function ToastMessage({ text, type }) {
+export default function ToastMessage({ message, onRemoveMessage }) {
+  function handleRemoveToast() {
+    onRemoveMessage(message.id)
+  }
+
   return (
-    <Container type={type}>
-      {type === "danger" && <img src={xCircleIcon} alt="Danger" />}
-      {type === "success" && <img src={checkCircleIcon} alt="Success" />}
-      <strong>{text}</strong>
+    <Container type={message.type} onClick={handleRemoveToast}>
+      {message.type === "danger" && <img src={xCircleIcon} alt="Danger" />}
+      {message.type === "success" && (
+        <img src={checkCircleIcon} alt="Success" />
+      )}
+      <strong>{message.text}</strong>
     </Container>
   )
 }
 
 ToastMessage.propTypes = {
-  text: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(["default", "danger", "success"])
-}
-
-ToastMessage.defaultProps = {
-  type: "dafault"
+  message: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["default", "danger", "success"])
+  }).isRequired,
+  onRemoveMessage: PropTypes.func.isRequired
 }
--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -1,35 +1,37 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {}
+    this.listeners = new Map()
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = []
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, [])
     }
 
-    this.listeners[event].push(listener)
+    this.listeners.get(event).push(listener)
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload)
     })
   }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners.get(event)
+
+    if (!listeners) {
+      return
+    }
+
+    const filterdListerners = listeners.filter(
+      (listener) => listener !== listenerToRemove
+    )
+
+    this.listeners.set(event, filterdListerners)
+  }
 }
-
-const toastEventManager = new EventManager()
-
-toastEventManager.on("addtoast", (payload) => {
-  console.log("addtoast listener 1", payload)
-})
-toastEventManager.on("addtoast", (payload) => {
-  console.log("addtoast listener 2", payload)
-})
-
-toastEventManager.emit("addtoast", { type: "danger", text: "texto" })
-console.log(toastEventManager)
