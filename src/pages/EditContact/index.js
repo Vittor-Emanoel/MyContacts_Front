050782b507ae7b02fb09547867bbd2a7bdb75e78
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-10-24 23:57:33 -0300

    feat: create method getContactById in service layer

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,12 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
 import PageHeader from '../../components/PageHeader';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  const {id} = useParams()
+  const history = useHistory()
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id)
+        console.log(contactData)
+        setIsLoading(false)
+      } catch (error) {
+        history.push('/')
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        })
+      }
+    }
+
+    loadContact()
+  }, [id, history])
+  
+
+
+   function handleSubmit() {
+    //
+
+  }
+
   return (
     <>
+
+    <Loader 
+      isLoading={isLoading}
+    />
       <PageHeader title="Editar Vittor Emanoel" />
 
-      <ContactForm buttonLabel="Salvar alterações" />
+      <ContactForm 
+        onSubmit={handleSubmit}
+        buttonLabel="Salvar alterações"
+      />
     </>
   );
 }
