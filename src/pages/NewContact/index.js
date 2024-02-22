e4f5a24849d1e48d26dfaee3bcbd9cdef36cfe1e
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2024-02-22 14:14:42 -0300

    refactor: newContact container presentation using react custom hooks

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,34 +1,9 @@
-import { useRef } from "react"
-import { useHistory } from "react-router-dom"
 import ContactForm from "../../components/ContactForm"
 import PageHeader from "../../components/PageHeader"
-import ContactsService from "../../services/ContactsService"
-import toast from "../../utils/toast"
+import useNewContact from "./useNewContact"
 
 export default function NewContact() {
-  const contactFormRef = useRef(null)
-  const history = useHistory()
-
-  async function handleSubmit(contact) {
-    try {
-      await ContactsService.createContact(contact)
-
-      contactFormRef.current.resetFields()
-
-      toast({
-        type: "success",
-        text: " Contato cadastrado com sucesso!",
-        duration: 3000
-      })
-
-      history.push("/")
-    } catch (error) {
-      toast({
-        type: "danger",
-        text: "Ocorreu um erro ao cadastrar o contato!"
-      })
-    }
-  }
+  const { handleSubmit, contactFormRef } = useNewContact()
 
   return (
     <>
---
+++ src/pages/NewContact/useNewContact.js
@@ -0,0 +1,34 @@
+import { useRef } from "react"
+import { useHistory } from "react-router-dom"
+import ContactsService from "../../services/ContactsService"
+import toast from "../../utils/toast"
+
+export default function useNewContact() {
+  const contactFormRef = useRef(null)
+  const history = useHistory()
+
+  async function handleSubmit(contact) {
+    try {
+      await ContactsService.createContact(contact)
+
+      contactFormRef.current.resetFields()
+
+      toast({
+        type: "success",
+        text: " Contato cadastrado com sucesso!",
+        duration: 3000
+      })
+
+      history.push("/")
+    } catch (error) {
+      toast({
+        type: "danger",
+        text: "Ocorreu um erro ao cadastrar o contato!"
+      })
+    }
+  }
+  return {
+    handleSubmit,
+    contactFormRef
+  }
+}
