48979592c208620e7823b251530d5c1566e3ceba
Vittor Emanoel <55211291+Vittor-Emanoel@users.noreply.github.com>
2024-05-13 09:12:31 -0300

    refactor: use createRef

--- src/hooks/useAnimatedList.js
+++ src/hooks/useAnimatedList.js
@@ -1,29 +1,45 @@
-import { useCallback, useState } from "react"
+import { createRef, useCallback, useRef, useState } from "react"
 
 export default function useAnimatedList(initialValue = []) {
   const [items, setItems] = useState(initialValue)
-
   const [pendingRemovalItemsIds, setPendingRemovalItemsIds] = useState([])
+
+  const animatedRefs = useRef(new Map)
 
   const handleRemoveItems = useCallback((id) => {
     setPendingRemovalItemsIds((prevState) => [...prevState, id])
   }, [])
 
-  const handleAnimationEnd = useCallback((id) => {
-    setItems((prevState) => prevState.filter((message) => message.id !== id))
-    setPendingRemovalItemsIds((prevState) =>
-      prevState.filter((messageId) => messageId !== id)
-    )
-  }, [])
+  // const handleAnimationEnd = useCallback((id) => {
+  //   setItems((prevState) => prevState.filter((message) => message.id !== id))
+  //   setPendingRemovalItemsIds((prevState) =>
+  //     prevState.filter((messageId) => messageId !== id)
+  //   )
+  // }, [])
+
+  const getAnimatedRef = useCallback((itemId) => {
+    let animatedRef = animatedRefs.current.get(itemId)
+
+    if(!animatedRef) {
+      animatedRef = createRef()
+      animatedRefs.current.set(itemId, animatedRef)
+    }
+
+    return animatedRef
+  }, []) 
 
   const renderList = useCallback(
     (renderItem) =>
-      items.map((item) =>
-        renderItem(item, {
-          isLeaving: pendingRemovalItemsIds.includes(item.id)
+      items.map((item) => {
+        const isLeaving = pendingRemovalItemsIds.includes(item.id)
+
+        const animatedRef = getAnimatedRef(item.id)
+        return renderItem(item, {
+          isLeaving,
+          animatedRef
         })
-      ),
-    [items, pendingRemovalItemsIds]
+      }),
+    [items, pendingRemovalItemsIds, getAnimatedRef]
   )
 
   return {
@@ -34,19 +50,4 @@ export default function useAnimatedList(initialValue = []) {
   }
 }
 
-// const animatedRef = useRef(null)
 
-//   useEffect(() => {
-//     function handleAnimationEnd() {
-//       onAnimationEnd(message.id)
-//     }
-
-//     const elementRefElement = animatedRef.current
-//     if (isLeaving) {
-//       elementRefElement.addEventListener("animationend", handleAnimationEnd)
-//     }
-
-//     return () => {
-//       elementRefElement.removeEventListener("animationend", handleAnimationEnd)
-//     }
-//   }, [isLeaving, message.id, onAnimationEnd])
